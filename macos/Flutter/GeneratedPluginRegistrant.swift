3e33f475fc4806f3d7826ccbda11ff0ba0dab59d
wangnan <paiooocn@qq.com>
2026-09-11 14:38:45 +0800

    ops: add CI + windows/macos platforms

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import shared_preferences_foundation
+import webview_all_wkwebview
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  WebviewAllWKWebViewPlugin.register(with: registry.registrar(forPlugin: "WebviewAllWKWebViewPlugin"))
+}
